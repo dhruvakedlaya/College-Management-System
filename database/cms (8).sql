1eac0e65d9d5be90418d470684a38d742b44297a
𝔇𝔥𝔯𝔲𝔳𝔞 𝕜𝕖𝕕𝕝𝕒𝕪𝕒 <96060375+dhruvakedlaya@users.noreply.github.com>
2026-01-29 23:56:36 +0530

    commit

--- database/cms (8).sql
+++ database/cms (8).sql
@@ -61,10 +61,7 @@ CREATE TABLE `course` (
 --
 
 INSERT INTO `course` (`sno`, `cname`, `full`, `fees`, `des`, `status`) VALUES
-(1, 'BCA', 'BACHELORS OF COMPUTER SCIENCE', 45000, 'dsjhfbdskjbfvdskjvbdsv\r\ndwesfiohdsfvonivdsv\r\ndsgvosihvbdsoivndsfv', 'available'),
-(2, 'BCOM', 'BACHELORS OF COMMERCE', 30000, 'Commerce Commerce', 'available'),
-(3, 'BSC', 'BACHELORS OF SCIENCE', 38000, 'Science', 'available'),
-(4, 'BBA', 'BACHELORS OF BUSINESS ADMINIST', 28000, 'BBA', 'available');
+(1, 'BCA', 'BACHELORS OF COMPUTER SCIENCE', 45000, 'dsjhfbdskjbfvdskjvbdsv\r\ndwesfiohdsfvonivdsv\r\ndsgvosihvbdsoivndsfv', 'available');
 
 -- --------------------------------------------------------
 
@@ -85,10 +82,7 @@ CREATE TABLE `feedback` (
 --
 
 INSERT INTO `feedback` (`fid`, `id`, `date`, `feedback`, `type`) VALUES
-(9, 'STAF230005', '2023-06-25', 'fDgfdf', 'staff'),
-(10, 'STUD230011', '2023-06-25', 'dgfbvvxcv', 'student'),
-(11, 'STUD230011', '2023-06-25', 'RTJGHRH', 'student'),
-(12, 'STAF230005', '2023-06-25', 'rthth', 'staff');
+(9, 'STAF230005', '2023-06-25', 'fDgfdf', 'staff');
 
 -- --------------------------------------------------------
 
@@ -109,13 +103,7 @@ CREATE TABLE `login` (
 INSERT INTO `login` (`type`, `id`, `pass`) VALUES
 ('admin', 'admin', 'pass'),
 ('staff', 'STAF230001', 'Abcd@1234'),
-('staff', 'STAF230003', 'STAF230003'),
-('staff', 'STAF230005', 'STAF230005'),
-('student', 'STUD230006', 'Abcd@1234'),
-('student', 'STUD230007', 'Abcd@1234'),
-('student', 'STUD230008', 'Abcd@1234'),
-('student', 'STUD230009', 'STUD230009'),
-('student', 'STUD230011', 'Abcd@12345');
+('student', 'STUD230006', 'Abcd@1234');
 
 -- --------------------------------------------------------
 
@@ -144,9 +132,7 @@ CREATE TABLE `marks` (
 --
 
 INSERT INTO `marks` (`sno`, `id`, `course`, `type`, `s1`, `s2`, `s3`, `s4`, `s5`, `s6`, `total`, `avg`, `grade`) VALUES
-(3, 'STUD230010', 'BCA', '2', 88, 55, 44, 88, 77, 66, 418, 69.67, 'first class'),
-(4, 'STUD230011', 'BCA', '1', 88, 55, 44, 88, 66, 77, 430, 71.6667, 'first class'),
-(5, 'STUD230011', 'BCA', '2', 100, 100, 66, 99, 100, 55, 520, 86.6667, 'distinction');
+(3, 'STUD230010', 'BCA', '2', 88, 55, 44, 88, 77, 66, 418, 69.67, 'first class');
 
 -- --------------------------------------------------------
 
@@ -165,10 +151,7 @@ CREATE TABLE `notice` (
 --
 
 INSERT INTO `notice` (`sno`, `date`, `notice`) VALUES
-(4, '2023-06-12', 'sxssss'),
-(6, '2023-06-12', 'oqhdisud'),
-(8, '2023-06-22', 'classes will be terminated early'),
-(9, '2023-06-25', 'wqdfhSADFIKANBFF');
+(4, '2023-06-12', 'sxssss');
 
 -- --------------------------------------------------------
 
@@ -190,8 +173,7 @@ CREATE TABLE `query` (
 --
 
 INSERT INTO `query` (`qid`, `id`, `type`, `query`, `date`, `response`) VALUES
-(4, 'STUD230011', 1, 'efeff', '2023-06-24', 'degfdsggdc'),
-(5, 'STUD230011', 2, 'sadfQSADASD', '2023-06-24', 'zvdvcvv');
+(4, 'STUD230011', 1, 'efeff', '2023-06-24', 'degfdsggdc');
 
 -- --------------------------------------------------------
 
@@ -220,10 +202,7 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`id`, `name`, `image`, `gender`, `dob`, `email`, `qual`, `depart`, `phone`, `address`, `status`, `pass`, `total_leave`) VALUES
-('STAF230001', 'Raj', 'images/1687501335_profile.png', 'Male', '2000-06-05', 'raj@email.com', 'BCA', 'BCOM', '9966335581', 'udupi 2', 'available', 'Abcd@1234', 0),
-('STAF230002', 'Raj', 'images/profile.png', 'Male', '2000-05-09', 'raj@email.com', 'BCA', 'BBA', '9966335588', 'udupi', 'Not Availa', 'Abcd@1234', 13),
-('STAF230003', 'Shreya', 'images/', 'Female', '2003-06-18', 'srjn@gmail.com', 'MCA', 'BCOM', '6789065432', 'Near Banashankari hardware', 'available', 'STAF230003', 15),
-('STAF230005', 'Adarsh', 'images/IMG-20230214-WA0015.jpg', 'Male', '2003-06-11', 'a@gmail.com', 'MCOM', 'BCA', '8899887766', 'bdfjvcjbvc', 'available', 'STAF230005', 4);
+('STAF230001', 'Raj', 'images/1687501335_profile.png', 'Male', '2000-06-05', 'raj@email.com', 'BCA', 'BCOM', '9966335581', 'udupi 2', 'available', 'Abcd@1234', 0);
 
 -- --------------------------------------------------------
 
@@ -246,11 +225,7 @@ CREATE TABLE `staff_leave` (
 --
 
 INSERT INTO `staff_leave` (`leave_id`, `id`, `type`, `date`, `num`, `reason`, `status`) VALUES
-(20, 'STAF230005', 'sick', '2023-06-25', 3, 'kujlikbh', 'rejected'),
-(21, 'STAF230005', 'annual', '2023-06-09', 1, 'hgfvhjv', 'approved'),
-(22, 'STAF230005', 'annual', '2023-06-23', 1, 'oihoh', 'approved'),
-(23, 'STAF230005', 'annual', '2023-06-15', 5, '7fkjygfv', 'approved'),
-(24, 'STAF230005', 'annual', '2023-06-23', 4, 'gfg', 'approved');
+(20, 'STAF230005', 'sick', '2023-06-25', 3, 'kujlikbh', 'rejected');
 
 -- --------------------------------------------------------
 
@@ -281,17 +256,7 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `name`, `image`, `email`, `fname`, `mname`, `phone`, `dob`, `gender`, `tenth`, `twlth`, `course`, `address`, `status`, `pass`) VALUES
-('STUD230001', 'riza', 'images/young-man.png', 'riza@gmail.com', '', '', '9944556688', '2023-06-13', 'Male', 88, 88, 'BCA', 'kundapura', 'Not Available', '123'),
-('STUD230002', 'hello', 'images/woman.png', 'd@gmail.com', '', '', '6655886633', '2023-06-10', 'Male', 65, 87, 'BCA', 'udupi', 'Not Available', '777'),
-('STUD230003', 'Rahul raju', 'images/1686840436_737400.jpg', 'ria@gmail.com', 'A', 'C', '5566889900', '1999-06-17', 'Male', 69, 79, 'BCA', 'Udupi', 'Not Available', 'Abcd@12345'),
-('STUD230004', 'Kumar', 'images/profile.png', 'k@gmail.com', '', '', '9955668844', '2023-06-02', 'Male', 55, 56, 'BCA', 'Bvr', 'Not Available', 'Abc@12345'),
-('STUD230005', 'vikas', 'images/profile.png', 'vikas@gmail.com', 'Bhoja', 'sujatha', '6655332244', '2003-06-11', 'Male', 45, 56, 'BSC', 'udupi', 'Not Available', 'Abcd@1234'),
-('STUD230006', 'karthik', 'images/bussiness-man.png', 'kar@gmail.com', 'fn', 'mn', '8866559966', '2023-06-08', 'Male', 45, 55, 'BBA', 'bvr', 'available', 'Abcd@1234'),
-('STUD230007', 'buddi', 'images/1687409251_woman (1).png', 'buddi@gmail.com', 'aaa', 'bbb', '5566889977', '2003-06-04', 'Female', 88, 76, 'BBA', 'kundapur', 'available', 'Abcd@1234'),
-('STUD230008', 'raj', 'images/profile.png', 'raj@gmail.com', 'xxx', 'yyy', '7788996655', '2003-06-06', 'Male', 55, 45, 'BSC', 'udupi', 'available', 'Abcd@1234'),
-('STUD230009', 'ayesha', 'images/woman.png', 'ayesha@gmail.com', 'Shekara', 'Shamala', '9966558844', '2001-06-14', 'Female', 45, 55, 'BSC', 'udupi', 'available', 'STUD230009'),
-('STUD230010', '2233445566', 'images/young-man.png', 'dr@gmail.com', '', '', '9955668844', '2023-06-24', 'Male', 88, 56, 'BCA', 'udupi', 'Not Available', 'STUD230010'),
-('STUD230011', 'Shrey', 'images/1687687435_peakpx.jpg', 'drv@gmail.com', '', '', '8899665588', '2023-06-23', 'Male', 88, 88, 'BCA', 'aaa', 'available', 'Abcd@12345');
+('STUD230001', 'riza', 'images/young-man.png', 'riza@gmail.com', '', '', '9944556688', '2023-06-13', 'Male', 88, 88, 'BCA', 'kundapura', 'Not Available', '123');
 
 --
 -- Indexes for dumped tables
